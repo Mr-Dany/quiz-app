87f6f31472fc9fb502799639058ee825593d374d
Mr-Dany <toshibaqosmiox75@hotmail.com>
2020-10-16 13:07:53 -0500

    Se agrego las preguntas al Scrip

---
+++ script.js
@@ -0,0 +1,77 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    },
+    {
+        question: 'What is the most used programming language in 2020',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    },
+    {
+        question: 'Who  is the President of US',
+        a: 'Florin Pop',
+        b: 'Donal Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    },
+    {
+        question: 'What does HTML stand for',
+        a: 'Hipertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    }const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    },
+    {
+        question: 'What is the most used programming language in 2020',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    },
+    {
+        question: 'Who  is the President of US',
+        a: 'Florin Pop',
+        b: 'Donal Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    },
+    {
+        question: 'What does HTML stand for',
+        a: 'Hipertext Markup Language',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    },
+    {
+        question: 'What year was Javascript launched',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'a'
+    }
+
+]
+
+]
